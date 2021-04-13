2c5cd5a9d9a0df7099fada5600e546a413636a86
fran-exfer <fran.exfer@gmail.com>
2021-04-13 19:12:26 +0200

    Add GET route to fetch a single user from the database.

--- routes.js
+++ routes.js
@@ -10,18 +10,34 @@ router.get('/users', async (req, res) => {
   res.send(users);
 });
 
+// Get a specific user
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    res.send(user);
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+});
+
 // Post a new user
 router.post('/users', async (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    fullname: req.body.fullname,
-    email: req.body.email,
-    address: req.body.address,
-    registrationDate: req.body.registrationDate,
-    status: req.body.status,
-  });
-  await user.save();
-  res.send(user);
+  try {
+    const user = new User({
+      username: req.body.username,
+      fullname: req.body.fullname,
+      email: req.body.email,
+      address: req.body.address,
+      registrationDate: req.body.registrationDate,
+      status: req.body.status,
+    });
+    await user.save();
+    res.send(user);
+  } catch {
+    res.status(500);
+    res.send({ error: 'There was an error in the POST request.' });
+  }
 });
 
 module.exports = router;
