6eeec008f62e8ee80bde2c3c77cc214ab093acae
fran-exfer <fran.exfer@gmail.com>
2021-04-14 19:33:21 +0200

    Try to fix CORS yet again (2).

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ mongoose
     const app = express();
     app.use(express.json());
 
-    app.use(cors()); // Use CORS middleware
-    app.options('*', cors()); // Allow CORS for all routes
+    //app.use(cors()); // Use CORS middleware
+
+    // Allow CORS
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+    });
 
     app.use('/api', routes);
 
